d9569a620ddcd3cd0ac2920af592645ae20aae98
Siddhi Shah <shahsiddhi777@gmail.com>
2024-03-08 15:15:54 +0530

    bottom nav done

--- navigations/Tabs.js
+++ navigations/Tabs.js
@@ -1,5 +1,5 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button,Image,TouchableOpacity } from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen1 from '../screens/ProfileScreen1';
 import FavouriteScreen from '../screens/FavouriteScreen';
@@ -9,16 +9,150 @@ import WalletScreen from '../screens/WalletScreen';
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => {
-  return (
-    <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        <Tab.Screen name="Profile" component={ProfileScreen1} options={{ headerShown: false }} />
-        <Tab.Screen name="Favourite" component={FavouriteScreen} options={{ headerShown: false }} />
-        <Tab.Screen name="History" component={HistoryScreen} options={{ headerShown: false }}  />
-        <Tab.Screen name="Wallet" component={WalletScreen} options={{ headerShown: false }} />
-    </Tab.Navigator>
+const CustomTabBarButton = ({children,onPress})=>{
+  return(
+    <TouchableOpacity
+    style={{
+      top:-30,
+      justifyContent:'center',
+      alignItems:'center',
+      ...styles.shadow
+    }}
+    onPress={onPress}
+    >
+      <View style={{
+        width:70,
+        height:70,
+        borderRadius:35,
+        backgroundColor:'#08B783'
+      }}>
+        {children}
+      </View>
+    </TouchableOpacity>
   )
 }
 
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={() => ({
+        tabBarShowLabel: false,
+        tabBarStyle: [
+          {
+            display: 'flex',
+            position: 'absolute',
+            // bottom: 25,
+            elevation: 0,
+            borderTopLeftRadius: 45,
+            borderTopRightRadius: 45,
+            height: 80,
+          },
+          null,
+        ],
+        
+      })}
+      
+
+      >
+     
+        <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false,
+        tabBarIcon:({focused})=>{
+          return(
+            <View style={{alignItems:'center',justifyContent:'center'}}>
+              <Image
+              source={require('../asset/icons/housei.png')}
+              resizeMode='contain'
+              style={{
+                width:35,
+                height:35,
+                tintColor:focused ? '#08B783' : 'gray'
+              }}
+              />
+              <Text style={{color:focused ? '#08B783' : 'gray',fontSize:12}}>Home</Text>
+            </View>
+          )
+        } }} />
+        <Tab.Screen name="Favourite" component={FavouriteScreen} options={{ headerShown: false,
+         tabBarIcon:({focused})=>{
+          return(
+            <View style={{alignItems:'center',justifyContent:'center'}}>
+              <Image
+              source={require('../asset/icons/hearti.png')}
+              resizeMode='contain'
+              style={{
+                width:35,
+                height:35,
+                tintColor:focused ? '#08B783' : 'gray'
+              }}
+              />
+              <Text style={{color:focused ? '#08B783' : 'gray',fontSize:12}}>Favourite</Text>
+            </View>
+          )
+        } }} />
+        <Tab.Screen name="Wallet" component={WalletScreen} options={{ headerShown: false,
+         tabBarIcon:({focused})=>(
+       
+           
+              <Image
+              source={require('../asset/icons/walleti.png')}
+              resizeMode='contain'
+              style={{
+                width:55,
+                height:55,
+                tintColor:'white',
+              }}
+              />
+  ),
+          tabBarButton:(props)=>(
+            <CustomTabBarButton {...props}/>
+         )
+        }} />
+        <Tab.Screen name="History" component={HistoryScreen} options={{ headerShown: false,
+         tabBarIcon:({focused})=>{
+          return(
+            <View style={{alignItems:'center',justifyContent:'center'}}>
+              <Image
+              source={require('../asset/icons/Timei.png')}
+              resizeMode='contain'
+              style={{
+                width:35,
+                height:35,
+                tintColor:focused ? '#08B783' : 'gray'
+              }}
+              />
+              <Text style={{color:focused ? '#08B783' : 'gray',fontSize:12}}>History</Text>
+            </View>
+          )
+        } }}  />
+        <Tab.Screen  name="Profile" component={ProfileScreen1} options={{ headerShown: false,
+         tabBarIcon:({focused})=>{
+          return(
+            <View style={{alignItems:'center',justifyContent:'center'}}>
+              <Image
+              source={require('../asset/icons/useri.png')}
+              resizeMode='contain'
+              style={{
+                width:35,
+                height:35,
+                tintColor:focused ? '#08B783' : 'gray'
+              }}
+              />
+              <Text style={{color:focused ? '#08B783' : 'gray',fontSize:12}}>Profile</Text>
+            </View>
+          )
+        } }} />
+    </Tab.Navigator>
+  )
+};
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    display: 'flex',
+    position: 'absolute',
+    elevation: 0,
+    borderTopLeftRadius: 45,
+    borderTopRightRadius: 45,
+    height: 80,
+  },
+});
 export default Tabs
